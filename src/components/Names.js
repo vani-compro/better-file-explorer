c86b43bbdeca9191cda5dc5c4bcf2e22a2448fd5
vani-compro <vani.sharma@comprotechnologies.com>
2023-10-23 16:56:19 +0530

    maintenance of fileStructure object

--- src/components/Names.js
+++ src/components/Names.js
@@ -1,10 +1,12 @@
-// import {Box} from '@mui/material';
+import AddButton from './AddButton';
 import {Button, Typography} from '@mui/material';
 import {IconButton} from '@mui/material';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import FolderIcon from '@mui/icons-material/Folder';
 import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
 import styled from 'styled-components';
+import { useState } from 'react';
 
 const FolderName = styled(Typography)`
   font-weight: 800 !important;
@@ -15,29 +17,40 @@ const FileName = styled(Typography)`
 `
 const WhiteIconButton = styled(IconButton)`
   color: #ddd !important;
+  display: inline !important;
 `
+const SpacedDiv = styled.div`
+  margin-left: 1.5rem;
+`
+export default function Names(props){ // type, name, fileStructure, setFileStructure
+  const [folderExpanded, setFolderExpanded] = useState(false);
 
-export default function Names(props){
+  const nameButtonClicked = () => {
+    setFolderExpanded(!folderExpanded);
+  }
+
   return (
-    <Button>
+    <>
       {props.type === "folder" ?
-        <>
-          <WhiteIconButton>
-            <ArrowForwardIosIcon />
-            <FolderIcon />
-          </WhiteIconButton>
-          <FolderName>{props.name}</FolderName>
-        </> :
-        <>
-        <WhiteIconButton>
-          <InsertDriveFileIcon />
-        </WhiteIconButton>
-        <FileName>{props.name}</FileName>
-        </>
+        <SpacedDiv>
+          <Button onClick={()=>nameButtonClicked()}>
+            <WhiteIconButton>
+              {folderExpanded ? <KeyboardArrowDownIcon /> : <KeyboardArrowRightIcon />}
+              <FolderIcon />
+            </WhiteIconButton>
+            <FolderName>{props.name}</FolderName>
+          </Button>
+          <AddButton fileStructure={props.fileStructure} setFileStructure={(val)=>props.setFileStructure(val)} folderExpanded={folderExpanded} setFolderExpanded={(val) => setFolderExpanded(val)}/>
+        </SpacedDiv> :
+        <SpacedDiv>
+          <Button>
+            <WhiteIconButton>
+              <InsertDriveFileIcon />
+            </WhiteIconButton>
+            <FileName>{props.name}</FileName>
+          </Button>
+        </SpacedDiv>
       }
-
-
-
-    </Button>
+    </>
   )
 }
